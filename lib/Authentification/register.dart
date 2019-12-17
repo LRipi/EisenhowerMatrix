41a0ac39480d79aa7443011f4b084251152c5976
Hugo <hugo.peuze@epitech.eu>
2019-12-17 14:46:26 +0100

    register style

--- lib/Authentification/register.dart
+++ lib/Authentification/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'login.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -10,7 +11,15 @@ class _RegisterPageState extends State<RegisterPage>
 {
   final _usernamecontroller = TextEditingController();
   final _passwordcontroller = TextEditingController();
+  final _passwordconfirmcontroller = TextEditingController();
+  bool error = false;
   
+  void setError(bool val) {
+    setState(() {
+      error = val;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold (
@@ -19,7 +28,7 @@ class _RegisterPageState extends State<RegisterPage>
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 5.0),
               child: TextField(
                 controller: _usernamecontroller,
                 decoration: new InputDecoration(
@@ -29,7 +38,7 @@ class _RegisterPageState extends State<RegisterPage>
                 )
             ),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 5.0),
               child: TextField(
                 obscureText: true,
                 controller: _passwordcontroller,
@@ -40,14 +49,23 @@ class _RegisterPageState extends State<RegisterPage>
               )
             ),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 5.0),
               child: TextField(
                 obscureText: true,
-                controller: _passwordcontroller,
+                controller: _passwordconfirmcontroller,
                 decoration: new InputDecoration(
                   labelText: "Confirm your password",
-                  icon: Icon(Icons.vpn_key),
+                  labelStyle: TextStyle(
+                    color: error ? Colors.red : Colors.grey
+                  ),
+                  icon: Icon(Icons.vpn_key, color: error ? Colors.red : Colors.grey),
                 ),
+                onChanged: (confirmPassword) {
+                  if (_passwordcontroller.text != _passwordconfirmcontroller.text)
+                    setError(true);
+                  else
+                    setError(false);
+                },
               )
             ),
             FlatButton(
