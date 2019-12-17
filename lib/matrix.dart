d49b4ef43280ad6abf3c279085d28ad012ab5082
Guillaume LE MARTRET <guillaume@lemartret.com>
2019-12-17 11:17:15 +0100

    Added Matrix page

---
+++ lib/matrix.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class MatrixPage extends StatefulWidget {
+  MatrixPage({Key key}) : super(key: key);
+
+  @override
+  _MatrixPageState createState() => _MatrixPageState();
+}
+
+class _MatrixPageState extends State<MatrixPage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Eisenhower Matrix'),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+          new Expanded(
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(16.0),
+                    child: MaterialButton(
+                      onPressed: () {},
+                      color: Colors.green,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Urgent'),
+                          Text('Pas Imporant'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                new VerticalDivider(width: 1.0),
+                new Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(16.0),
+                    child: MaterialButton(
+                      onPressed: () {},
+                      color: Colors.orange,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Urgent'),
+                          Text('Important'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Divider(height: 1.0),
+          new Expanded(
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(16.0),
+                    child: MaterialButton(
+                      onPressed: () {},
+                      color: Colors.blue,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Pas urgent'),
+                          Text('Pas Important'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                new VerticalDivider(width: 1.0),
+                new Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(16.0),
+                    child: MaterialButton(
+                      onPressed: () {},
+                      color: Colors.yellow,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Pas urgent'),
+                          Text('Important'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: Icon(Icons.add),
+      // ),
+    );
+  }
+}
