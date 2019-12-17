173f6daa08bfdbe8a8e2e216710f6be06b0d295e
Hugo <hugo.peuze@epitech.eu>
2019-12-17 11:14:15 +0100

    add some style on login page

--- lib/Authentification/login.dart
+++ lib/Authentification/login.dart
@@ -18,26 +18,61 @@ class _LoginPageState extends State<LoginPage>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-              TextField(
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+              child: TextField(
                 controller: _usernamecontroller,
                 decoration: new InputDecoration(
-                  labelText: "Enter your username",
-                  
-                ),
-              ),
-              TextField(
+                    labelText: "Enter your username",
+                    icon: Icon(Icons.person),
+                  ),
+                )
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+              child: TextField(
+                obscureText: true,
                 controller: _passwordcontroller,
-                decoration: new InputDecoration(labelText: "Enter your password"),
-              ),
-              FlatButton(
-                onPressed: () {
-                  debugPrint('Username: ' + _usernamecontroller.text + '\nPassword: ' + _passwordcontroller.text);
-                },
-                child: Text('Login'),
+                decoration: new InputDecoration(
+                  labelText: "Enter your password",
+                  icon: Icon(Icons.vpn_key),
+                ),
               )
-            ], 
-          ),
-        )
+            ),
+            FlatButton(
+              onPressed: () {
+                debugPrint('Username: ' + _usernamecontroller.text + '\nPassword: ' + _passwordcontroller.text);
+              },
+              child: Text('Login'),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 50.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Not registered yet ?',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    )
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      debugPrint('I will register');
+                    },
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Theme.of(context).accentColor,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  )
+                ],
+              )
+            ),
+          ], 
+        ),
+      )
     );
   }
 }
